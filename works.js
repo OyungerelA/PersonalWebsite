13454418ecce93419010a96ee763c4856683b61d
OyungerelA <79796327+OyungerelA@users.noreply.github.com>
2021-07-08 18:16:55 +0800

    Add files via upload

---
+++ works.js
@@ -0,0 +1,57 @@
+document.getElementById("box1").addEventListener("click", ()=>{
+	document.getElementById("tab1").style.display = "block"
+	document.getElementById("page1").style.display = "block"
+	document.getElementById("box1").style.opacity = "0.6";
+	document.getElementById("box4").style.opacity = "0.6";
+})
+
+document.getElementById("close").addEventListener("click", ()=>{
+	document.getElementById("tab1").style.display = "none";
+	document.getElementById("page1").style.display = "none"
+	document.getElementById("box1").style.opacity = "1";
+	document.getElementById("box4").style.opacity = "1";
+})
+
+document.getElementById("box2").addEventListener("click", ()=>{
+	document.getElementById("tab1").style.display = "block"
+	document.getElementById("page2").style.display = "block"
+	document.getElementById("box1").style.opacity = "0.6";
+	document.getElementById("box4").style.opacity = "0.6";
+})
+
+document.getElementById("close").addEventListener("click", ()=>{
+	document.getElementById("tab1").style.display = "none";
+	document.getElementById("page2").style.display = "none"
+	document.getElementById("box1").style.opacity = "1";
+	document.getElementById("box4").style.opacity = "1";
+})
+
+document.getElementById("box3").addEventListener("click", ()=>{
+	document.getElementById("tab1").style.display = "block"
+	document.getElementById("page3").style.display = "block"
+	document.getElementById("box1").style.opacity = "0.6";
+	document.getElementById("box4").style.opacity = "0.6";
+})
+
+document.getElementById("close").addEventListener("click", ()=>{
+	document.getElementById("tab1").style.display = "none";
+	document.getElementById("page3").style.display = "none"
+	document.getElementById("box1").style.opacity = "1";
+	document.getElementById("box4").style.opacity = "1";
+})
+
+document.getElementById("box4").addEventListener("click", ()=>{
+	document.getElementById("tab1").style.display = "block"
+	document.getElementById("page4").style.display = "block"
+	document.getElementById("box1").style.opacity = "0.6";
+	document.getElementById("box4").style.opacity = "0.6";
+})
+
+document.getElementById("close").addEventListener("click", ()=>{
+	document.getElementById("tab1").style.display = "none";
+	document.getElementById("page4").style.display = "none"
+	document.getElementById("box1").style.opacity = "1";
+	document.getElementById("box4").style.opacity = "1";
+})
+
+
